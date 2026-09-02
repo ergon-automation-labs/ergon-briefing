d5e7c85950c58377574a89099063bd0cf9423c42
Abby Malson <august.malson@gmail.com>
2026-09-02 09:49:16 +0300

    chore: Bump version to 0.1.7 to pick up runtime v0.14.66

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BotArmyBriefingBot.MixProject do
     [
       app: :bot_army_briefing_bot,
 <<<<<<< HEAD
-      version: "0.1.6",
+      version: "0.1.7",
 =======
       version: "0.1.8",
 >>>>>>> 61f6527 (chore: Bump version to 0.1.8 to pick up runtime v0.14.62)
